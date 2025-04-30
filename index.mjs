046344497003be6e711015efcb698f6942813717
Anthony Constante <85783762+aconst26@users.noreply.github.com>
2025-04-30 13:39:01 -0700

    Added Login Route
    Installed other packages like session and bcrypt to deal with the login process.

--- index.mjs
+++ index.mjs
@@ -1,15 +1,22 @@
 import express from 'express';
 import mysql from 'mysql2/promise';
+import bcrypt from 'bcrypt';
+import session from 'express-session';
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-//for Express to get values using POST method
 app.use(express.urlencoded({extended:true}));
 
-//setting up database connection pool
+app.set('trust proxy', 1) 
+app.use(session({
+  secret: 'cst336 csumb',
+  resave: false,
+  saveUninitialized: true
+}))
+
 const pool = mysql.createPool({
     host: "ant-constante.online",
     user: "antcons1_finaluser",
@@ -18,18 +25,38 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     waitForConnections: true
 });
+
 const conn = await pool.getConnection();
 
-//routes
 app.get('/', (req, res) => {
    res.render('home.ejs')
 });
+
+app.post('/login', async(req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    let hashPassword;
+    let sql = "SELECT * FROM users WHERE username = ?";
+    const [rows] = await conn.query(sql, [username]);
+    if(rows.length > 0) {
+        hashPassword = rows[0].password;
+    }
+    const match = await bcrypt.compare(password, hashPassword);
+    if(match) {
+        req.session.userAuthenticated = true;
+        req.session.fullName = rows[0].firstName + ' ' + rows[0].lastName;
+        res.render('home.ejs')
+    } else {
+        res.render('login.ejs', {"error":"Wrong credentials!"})
+    }
+})
+
 
 app.get("/dbTest", async(req, res) => {
     let sql = "SELECT CURDATE()";
     const [rows] = await conn.query(sql);
     res.send(rows);
-});//dbTest
+});
 
 app.listen(3000, ()=>{
     console.log("Express server running")
